c9ec96dfca2d00c68cfd1c6563bc021427baf4c0
VeyronSakai <yuukisakai123@gmail.com>
2020-12-31 02:00:59 +0900

    Prefabを削除できるように

---
+++ com.veyronsakai.prefabgenerator/PrefabGenerator/PrefabDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PrefabGenerator
+{
+    public static class PrefabDestroyer
+    {
+        public static void Destroy<T>(ref T prefab) where T : PrefabBase
+        {
+            if (prefab == null)
+                return;
+
+            Object.Destroy(prefab.gameObject);
+            prefab = null;
+        }
+    }
+}
