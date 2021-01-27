bcb5b95b271a452a66f60477fb8bb36f4ca40fa2
VeyronSakai <yuukisakai123@gmail.com>
2021-01-28 01:33:53 +0900

    commit

--- com.veyronsakai.prefabgenerator/PrefabGenerator/PrefabFactory.cs
+++ com.veyronsakai.prefabgenerator/PrefabGenerator/PrefabFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace PrefabGenerator
 {
@@ -7,14 +8,9 @@ namespace PrefabGenerator
     {
         public static T Create<T>(string prefabPath) where T : PrefabBase
         {
-            var obj = Resources.Load(prefabPath);
+            var obj = LoadPrefab(prefabPath);
 
-            if (obj == null)
-            {
-                throw new ArgumentException($"Prefab does not exist in the path {prefabPath}");
-            }
-
-            var gameObject = UnityEngine.Object.Instantiate(obj) as GameObject;
+            var gameObject = Object.Instantiate(obj) as GameObject;
 
             if (gameObject == null)
             {
@@ -33,14 +29,9 @@ namespace PrefabGenerator
 
         public static T Create<T>(string prefabPath, Transform parent) where T : PrefabBase
         {
-            var obj = Resources.Load(prefabPath);
+            var obj = LoadPrefab(prefabPath);
 
-            if (obj == null)
-            {
-                throw new ArgumentException($"Prefab does not exist in the path {prefabPath}");
-            }
-
-            var gameObject = UnityEngine.Object.Instantiate(obj, parent) as GameObject;
+            var gameObject = Object.Instantiate(obj, parent) as GameObject;
 
             if (gameObject == null)
             {
@@ -55,6 +46,39 @@ namespace PrefabGenerator
             }
 
             return instance;
+        }
+
+        public static T Create<T>(string prefabPath, Transform parent, Vector3 position) where T : PrefabBase
+        {
+            var obj = LoadPrefab(prefabPath);
+
+            var gameObject = UnityEngine.Object.Instantiate(obj, position, Quaternion.identity, parent) as GameObject;
+
+            if (gameObject == null)
+            {
+                throw new InvalidCastException();
+            }
+
+            var instance = gameObject.GetComponent<T>();
+
+            if (instance == null)
+            {
+                throw new ArgumentException($"Script {typeof(T)} is not attached to {prefabPath}");
+            }
+
+            return instance;
+        }
+
+        private static Object LoadPrefab(string prefabPath)
+        {
+            var obj = Resources.Load(prefabPath);
+
+            if (obj == null)
+            {
+                throw new ArgumentException($"Prefab does not exist in the path {prefabPath}");
+            }
+
+            return obj;
         }
     }
 }
